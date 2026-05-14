29342fa5bf00f372daf5f9fcacbc4a1b143de708
kamal <k35x@proton.me>
2026-05-14 17:58:23 +0700

    tampilkan status pesanan dari database

--- app/config/database.php
+++ app/config/database.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'port' => 3306,
-    'database' => 'si_siblings',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8mb4',
-    'options' => [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => 3306,
+    "database" => "si_siblings",
+    "username" => "root",
+    "password" => "",
+    "charset" => "utf8mb4",
+    "options" => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
--- app/core/Model.php
+++ app/core/Model.php
@@ -3,4 +3,26 @@
 class Model
 {
     protected array $data = [];
+    protected PDO $db;
+
+    public function __construct()
+    {
+        $config = require __DIR__ . '/../config/database.php';
+
+        $dsn = sprintf(
+            '%s:host=%s;port=%d;dbname=%s;charset=%s',
+            $config['driver'],
+            $config['host'],
+            $config['port'],
+            $config['database'],
+            $config['charset']
+        );
+
+        $this->db = new PDO(
+            $dsn,
+            $config['username'],
+            $config['password'],
+            $config['options']
+        );
+    }
 }
